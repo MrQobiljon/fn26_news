1597c73fa197de0322766dbbefeb2887bf6f9648
Qobiljon <madrahimovq@gmail.com>
2024-12-06 18:52:29 +0500

    admin panelda ishlash 7-kun

--- config/news/admin.py
+++ config/news/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Category, Post
 
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'views', 'created', 'updated', 'category', 'published', 'get_image')
+    list_display_links = ('name', 'pk')
+    list_editable = ('published', 'category')
+    list_filter = ('category', 'published')
+    search_fields = ('pk', 'name', 'content')
+    list_per_page = 10
+    readonly_fields = ('views',)
+
+    def get_image(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" width="150px">')
+        else:
+            return '-'
+
+    get_image.short_description = "Rasmi"
+
+
+admin.site.register(Post, PostAdmin)
--- config/news/models.py
+++ config/news/models.py
@@ -4,21 +4,31 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name="Kategoriya nomi")
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Kategoriya"
+        verbose_name_plural = 'Kategoriyalar'
 
 
 class Post(models.Model):
-    name = models.CharField(max_length=255)
-    content = models.TextField(blank=True, null=True)
-    photo = models.ImageField(upload_to="post/photos/", blank=True, null=True)
-    views = models.IntegerField(default=0)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    published = models.BooleanField(default=True)
+    name = models.CharField(max_length=255, verbose_name="Maqola nomi")
+    content = models.TextField(blank=True, null=True, verbose_name="Maqola matni")
+    photo = models.ImageField(upload_to="post/photos/", blank=True, null=True, verbose_name="Rasmi")
+    views = models.IntegerField(default=0, verbose_name="Ko'rishlar soni")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Qo'shilgan vaqti")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Yangilangan vaqti")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Kategoriya")
+    published = models.BooleanField(default=True, verbose_name="Saytga chiqarish",
+                                    help_text="Agar galochka qo'ysangiz sayta chiqaradi!!!")
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Maqola'
+        verbose_name_plural = 'Maqolalar'
+        ordering = ['-created', '-pk']
