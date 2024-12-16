38267ce2c56a4f41112821cb0652f0852861b73c
Qobiljon <madrahimovq@gmail.com>
2024-12-16 19:07:25 +0500

    11-kun

--- config/news/views.py
+++ config/news/views.py
@@ -31,6 +31,10 @@ def posts_by_category(request, category_id):
 
 def post_detail(request: WSGIRequest, pk):
     post = get_object_or_404(Post, pk=pk)
+
+    post.views += 1
+    post.save()
+
     context = {
         "post": post,
         "title": post.name
@@ -50,3 +54,47 @@ def add_post(request: WSGIRequest):
         "form": form
     }
     return render(request, 'add_post.html', context)
+
+
+def update_post(request: WSGIRequest, pk: int):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = PostForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            post.name = form.cleaned_data.get("name")
+            post.content = form.cleaned_data.get("content")
+            post.photo = form.cleaned_data.get("photo") if form.cleaned_data.get("photo") else post.photo
+            post.category = form.cleaned_data.get("category")
+            post.published = form.cleaned_data.get("published")
+            post.video = form.cleaned_data.get("video") if form.cleaned_data.get("video") else post.video
+            post.save()
+            return redirect('detail', pk=post.pk)
+
+    form = PostForm(initial={
+        "name": post.name,
+        "content": post.content,
+        "photo": post.photo,
+        "category": post.category,
+        "published": post.published
+    })
+
+    context = {
+        "form": form,
+        'post': post
+    }
+
+    return render(request, 'add_post.html', context)
+
+
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+
+    context = {
+        "post": post
+    }
+    return render(request, 'confirm_delete.html', context)
+
